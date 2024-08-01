819e651bb7a0592cb99aa167edadf91c49c34d9d
Junzhang Liu <ayhexinf@gmail.com>
2024-08-01 16:15:40 -0400

    Update website

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ window.HELP_IMPROVE_VIDEOJS = false;
 
 var INTERP_BASE = "./static/interpolation/stacked";
 var NUM_INTERP_FRAMES = 240;
-
+var TABLE_CONTENT_BASE = "./static/tables/table_content.json";
 var interp_images = [];
 function preloadInterpolationImages() {
   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
@@ -17,6 +17,78 @@ function setInterpolationImage(i) {
   image.ondragstart = function() { return false; };
   image.oncontextmenu = function() { return false; };
   $('#interpolation-image-wrapper').empty().append(image);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  fetch("./static/tables/table_content.json")
+      .then(response => response.json())
+      .then(data => {
+          populateTable(data);
+          sortTable(0,true); 
+      })
+      .catch(error => console.error('Error loading data:', error));
+});
+
+function populateTable(data) {
+  let tableBody = document.querySelector('#leaderboard tbody');
+  let entries = document.querySelector('#leaderboard thead').innerText.split("\t")
+  tableBody.innerHTML = '';  // Clear existing content
+  data.forEach(item => {
+      let row = document.createElement('tr');
+
+      entries.forEach(entry => {
+          let cell = document.createElement('td');
+          cell.textContent = item[entry];
+          row.appendChild(cell);
+      });
+
+      tableBody.appendChild(row);
+  });
+}
+
+function sortTable(n,initialSort = false) {
+  let table = document.getElementById("leaderboard");
+  let rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  switching = true;
+  dir = "asc"; // Set the sorting direction to ascending initially
+
+  table.querySelectorAll("th").forEach(th => th.classList.remove("sorted-asc", "sorted-desc"));
+
+  while (switching) {
+      switching = false;
+      rows = table.rows;
+      
+      for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+          let xContent = x.textContent.trim();
+          let yContent = y.textContent.trim();
+          if (!isNaN(xContent) && !isNaN(yContent)) {
+            xContent = parseFloat(xContent);
+            yContent = parseFloat(yContent);
+        }
+          if (dir == "asc") {
+              if (xContent > yContent) {
+                  shouldSwitch = true;
+              }
+          } else if (dir == "desc") {
+              if (xContent < yContent) {
+                  shouldSwitch = true;
+              }
+          }
+          if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        }
+      }
+      if (switchcount === 0 && dir === "asc" && !initialSort) {
+            dir = "desc";
+            switching = true;
+      }
+  }
+  table.querySelector(`th:nth-child(${n + 1})`).classList.add(dir === "asc" ? "sorted-asc" : "sorted-desc");
+
 }
 
 
@@ -57,7 +129,6 @@ $(document).ready(function() {
     		console.log(state);
     	});
     }
-
     /*var player = document.getElementById('interpolation-video');
     player.addEventListener('loadedmetadata', function() {
       $('#interpolation-slider').on('input', function(event) {
@@ -65,14 +136,13 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
+    // preloadInterpolationImages();
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    // $('#interpolation-slider').on('input', function(event) {
+    //   setInterpolationImage(this.value);
+    // });
+    // setInterpolationImage(0);
+    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
-    bulmaSlider.attach();
-
+    // bulmaSlider.attach();
 })
